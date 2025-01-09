4dbb5028d9835616bd120a5a68c13e9e5e2f56d6
ANCLIN Léo <l.anclin@enovea-infra.fr>
2025-01-09 16:40:23 +0100

    modif fichier init.sh

--- init.sh
+++ init.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
+
 # Authentification avec Google Cloud
 gcloud auth login
+gcloud auth application-default login
+
 
 # Demande à l'utilisateur de saisir l'ID de l'organisation
 read -p "Entrez l'ID de votre organisation (ORG_ID): " ORG_ID
 
+# Ajout de l'utilisateur à l'organisation
+read -p "Entrez l'email de votre compte : " EMAIL
+gcloud organizations add-iam-policy-binding $ORG_ID \
+    --member="user:$EMAIL" \
+    --role="roles/resourcemanager.organizationAdmin"
+
 # Demande à l'utilisateur de saisir l'ID du projet à créer
-read -p "Entrez l'ID du projet à créer (YOUR_PROJECT_ID): " PROJECT_ID
+read -p "Entrez l'ID des projet à créer (YOUR_PROJECT_ID): " PROJECT_ID
 
 # Demande à l'utilisateur de saisir le nom du projet
-read -p "Entrez le nom du projet (Your Project Name): " PROJECT_NAME
-
-# Création du projet GCP
-# gcloud projects create $PROJECT_ID --name="$PROJECT_NAME" --organization=$ORG_ID
+read -p "Entrez le nom des projet (Your Project Name): " PROJECT_NAME
 
 # Mettre dans des variables ORG_ID PROJECT_ID et PROJECT_NAME
 export ORG_ID=$ORG_ID
